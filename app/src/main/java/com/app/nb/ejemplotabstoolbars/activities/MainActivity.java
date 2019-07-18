73e918ff97f6802331c2d26470a4048701c8643a
namelessbliss <anonimo2911.2012@gmail.com>
2019-07-18 12:07:10 -0500

    Tabs Ejemplo

--- app/src/main/java/com/app/nb/ejemplotabstoolbars/activities/MainActivity.java
+++ app/src/main/java/com/app/nb/ejemplotabstoolbars/activities/MainActivity.java
@@ -1,9 +1,15 @@
-package com.app.nb.ejemplotabstoolbars;
+package com.app.nb.ejemplotabstoolbars.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Toast;
+
+import com.app.nb.ejemplotabstoolbars.R;
+import com.app.nb.ejemplotabstoolbars.adapter.PagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +21,48 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Estabece tabs necesarias
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+
+        // Establece adaptador del ViewPager
+        final ViewPager viewPager = findViewById(R.id.viewPager);
+
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager(), 3);
+
+        viewPager.setAdapter(adapter);
+        //Cuando agamos un cambio de pagina
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, "Seleccionado" + tab.getText(), Toast.LENGTH_SHORT).show();
+                int position = tab.getPosition();
+                viewPager.setCurrentItem(position);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, "No seleccionado " + tab.getText(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, "Reseleccionado " + tab.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
 
     }
--- app/src/main/java/com/app/nb/ejemplotabstoolbars/adapter/PagerAdapter.java
+++ app/src/main/java/com/app/nb/ejemplotabstoolbars/adapter/PagerAdapter.java
@@ -1,4 +1,44 @@
 package com.app.nb.ejemplotabstoolbars.adapter;
 
-public class PagerAdapter {
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.app.nb.ejemplotabstoolbars.fragments.FirstFragment;
+import com.app.nb.ejemplotabstoolbars.fragments.SecondFragment;
+import com.app.nb.ejemplotabstoolbars.fragments.ThirdFragment;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+
+    // numero de tabs
+    private int numbreOfTabs;
+
+
+    public PagerAdapter(FragmentManager fm, int numbreOfTabs) {
+        super(fm);
+        this.numbreOfTabs = numbreOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+
+        //Depediendo del la posicion
+        //Se laza en cada swipe o click
+        switch (position) {
+            case 0:
+                return new FirstFragment();
+            case 1:
+                return new SecondFragment();
+            case 2:
+                return new ThirdFragment();
+            default:
+                return null;
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return numbreOfTabs;
+    }
 }
