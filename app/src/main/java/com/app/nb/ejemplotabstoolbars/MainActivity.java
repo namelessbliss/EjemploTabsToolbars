c7f9dbc4446533747bbe6af5857c5d6d8bfd9287
namelessbliss <anonimo2911.2012@gmail.com>
2019-07-18 11:21:25 -0500

    Toolbar Mejorado

---
+++ app/src/main/java/com/app/nb/ejemplotabstoolbars/MainActivity.java
@@ -0,0 +1,26 @@
+package com.app.nb.ejemplotabstoolbars;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+
+    }
+}
